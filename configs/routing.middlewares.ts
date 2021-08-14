eaba2222084d87d857c621b4b726ef2eb84f40eb
pin84 <qp1984@live.com>
2021-08-14 11:01:59 +0800

    ave

--- configs/routing.middlewares.ts
+++ configs/routing.middlewares.ts
@@ -11,8 +11,8 @@ export class HeaderMiddleware implements KoaMiddlewareInterface {
     // console.log('---aaa-',context);
 
     context.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH')
-    context.set('Access-Control-Allow-Origin', context.request.header.origin)
-    // context.set('Access-Control-Allow-Origin', 'http://localhost:8080/')
+    // context.set('Access-Control-Allow-Origin', context.request.header.origin)
+    context.set('Access-Control-Allow-Origin', 'http://localhost:8080/')
     context.set('Access-Control-Allow-Headers', ['content-type'])
     context.set('Access-Control-Allow-Credentials', 'true')
     context.set('Content-Type', 'application/json; charset=utf-8')
